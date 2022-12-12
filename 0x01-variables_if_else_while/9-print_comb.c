8035622f04e87c57ec56d9addd41d8643123582c
ekene <ekeneugwuejim@gmail.com>
2022-12-12 13:32:58 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+ /**
+ * main - Prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
