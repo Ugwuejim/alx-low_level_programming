331cafa73ddff7e5edfa33c7997c6c50746571db
ekene <ekeneugwuejim@gmail.com>
2022-12-12 12:42:05 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - alphabets in lower cases follow by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l_letter = 'a';
+
+	while (l_letter <= 'z')
+	{
+		if (!(l_letter == 'e' || l_letter == 'q'))
+		{
+			putchar(l_letter);
+		}
+
+		l_letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
